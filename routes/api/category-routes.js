80b4b93f52240e1c79820610adf12d48e62b6e2f
Terrance Smith <terrancesmith@Laquishas-MacBook-Air.local>
2023-01-26 17:31:47 -0500

    added to product routes page

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -10,7 +10,7 @@ router.get('/', async (req, res) => {
     const categoryData = await Category.findAll();
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500).json(err)    
+    res.status(500).json(err)
   }
 });
 
@@ -61,7 +61,7 @@ router.delete('/:id', async (req, res) => {
     }
     res.status(200).json(categoryData);
   } catch (err) {
-    res.status(500).json(err);
+    res.status(500).json(err)
   }
 });
 
